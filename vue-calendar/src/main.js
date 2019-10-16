68060e83401670f438ff30900aa8f375bd724947
steampunklink <nethhorn@gmail.com>
2019-10-15 21:28:33 -0700

    calendar is working

--- vue-calendar/src/main.js
+++ vue-calendar/src/main.js
@@ -1,10 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-Vue.config.productionTip = false
+import fbdata from "./fbdata.js";
+
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp(fbdata);
+
+export const db = firebase.firestore();
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
